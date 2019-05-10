bd2e01d71e7f29196be4e1980bf920f72c395796
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-05-10 15:43:45 -0600

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  arr.sort_by do |word|
+  	word.split('').map do |char|
+		esperanto.index(char)
+  	end
+
+  end
+
 end
